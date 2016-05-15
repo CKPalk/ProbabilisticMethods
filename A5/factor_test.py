f1ef65c88260502759bfd157a6f4a414721b6aac
Cameron Palk <ckpalk@comcast.net>
2016-05-15 14:42:39 -0700

    Created starter code for variable elimination

--- A5/factor_test.py
+++ A5/factor_test.py
@@ -1,136 +1,110 @@
 # CIS 410/510pm
-# Homework #4
-# Daniel Lowd
-# April 2016
+# Homework 5 beta 0.0.1
+# Cameron Palk
+# May 2016
 #
-# TEMPLATE CODE
+# Special thanks to Daniel Lowd for the skeletor code
+
 import sys
 import tokenize
 from functools import reduce
 
-#
-# FACTOR CLASS -- EDIT HERE!
-#
 
 global_card = []
+num_vars = 0
 
-
-def pVal( val ):
-	return round( val, 3 )
-
-def printableVals( vals ):
-	return [ pVal( v ) for v in vals ]
-
-def cardinality( A ):
-	return len( A )
-
-def cardinalityOfValues( XUX ):
-	return reduce( lambda agg,x: agg * global_card[x], XUX, 1 )
-
-
-def cardOfUnion( U ):
-	return { u : global_card[ u ] for u in U }
-
-def union( A, B ):
-    AUB = [ a for a in A ]
-    for b in B:
-        if b not in A:
-            AUB.append( b )
-    print( "A:", A, "union B:", B, "=", AUB )
-    return AUB
-
+''' Calc Strides
+'''
 def calcStrides( scope ):
 	rev_scope = list( reversed( scope ) )
-	res = [ 0 ] * len( scope )
-	res[ 0 ] = 1
+	res = [ 1 ] + [ 0 ] * ( len( scope ) - 1 )
 	for idx in range( 1, len( rev_scope ) ):
 		res[ idx ] = res[ idx - 1 ] * global_card[ rev_scope[ idx - 1 ] ]
 	stride = list( reversed( res ) )
-	#return { scope[i] : res[i]	  for i in range( len( scope ) ) }
 	return { scope[i] : stride[i] for i in range( len( scope ) ) }
 
-
-class Factor(dict):
+# FACTOR CLASS DEFINITION
+class Factor( dict ):
+	# Constructor
 	def __init__(self, scope_, vals_):
 		self.scope = scope_
 		self.vals = vals_
 		self.stride = calcStrides( scope_ )
-		return
 	#
 
+	# Are two object EQual, True of False
+	def __eq__(self, other):
+		return (self.scope  == other.scope and
+				self.vals   == other.vals  and
+				self.stride == other.stride )
+	#
+
+	# A string used for printing the Factor Objects
 	def __repr__( self ):
-		style = "\n{0}\nScope: {1}\nStride: {2}\nVals:\n{3}\n{0}\n"
-		vertBar = str( ['-'] * 50 ) )
+		style = "\n{0}\nScope: {1}\nStride: {2}\nCard: {3}\nVals:\n{4}\n{0}\n"
+		vertBar = ''.join( ['-'] * 50 )
 		return style.format( vertBar, self.scope, self.stride,
+							 { v : global_card[v] for v in self.scope },
 							 '\n'.join( [ str( round( e, 3 ) ) for e in self.vals ] ) )
 	#
 
-	def __mul__(self, other):
+	# What the '*' character does between our objects
+	def __mul__( self, other ):
+		new_scope 	= list( set( self.scope ).union( set( other.scope ) ) )
+		assignment 	= { e : 0 for e in new_scope }
+		card 		= { u : global_card[ u ] for u in new_scope }
+		val_count	= reduce( lambda agg, x: agg * global_card[x], new_scope, 1 )
+		new_vals 	= [ 0 ] * val_count
 
-		phi1_stride = self.stride
-		phi2_stride = other.stride
-
-		idx1 = 0
-		idx2 = 0
-
-		XUX = union( self.scope, other.scope )
-
-		# This creates an array to store all vals
-		psi = [ 0 ] * cardinalityOfValues( XUX )
-
-		# Assignment [ 0's x Number of RVs in union ]
-		assignment = { e : 0 for e in XUX }
-
-		card_vals = cardinalityOfValues( XUX )
-		card_XUX  = cardinality( XUX )
-		card = cardOfUnion( XUX )
-
-		# This counts up 0, 1, ..., the number of possible values
-		for i in range( 0, card_vals ):
-			psi[ i ] = self.values[ idx1 ] * other.values[ idx2 ]
-			for l in reversed( XUX ):
-				assignment[ l ] += 1
-				if assignment[ l ] == card[ l ]:
-					assignment[ l ] = 0
-					idx1 -= (( card[ l ] - 1 ) * self.stride [ l ] ) if l in self.scope  else 0
-					idx2 -= (( card[ l ] - 1 ) * other.stride[ l ] ) if l in other.scope else 0
+		idx1 = idx2 = 0
+		for i in range( 0, val_count ):
+			new_vals[ i ] = self.vals[ idx1 ] * other.vals[ idx2 ]
+			for rv in reversed( new_scope ):
+				if assignment[ rv ] == card[ rv ] - 1:
+					idx1 -= assignment[ rv ] * self.stride [ rv ] if rv in self.stride  else 0
+					idx2 -= assignment[ rv ] * other.stride[ rv ] if rv in other.stride else 0
+					assignment[ rv ] = 0
 				else:
-					idx1 += self.stride [ l ] if l in self.scope  else 0
-					idx2 += other.stride[ l ] if l in other.scope else 0
+					idx1 += self.stride [ rv ] if rv in self.scope  else 0
+					idx2 += other.stride[ rv ] if rv in other.scope else 0
+					assignment[ rv ] += 1
 					break
-
-		return Factor( XUX, psi )
+		#
+		return Factor( new_scope, new_vals )
 	#
 
-	def __rmul__(self, other):
-		return self * other
+	# Sum out the variable and return a new Factor
+	def sumOut( self ):
+		# TODO Sum out a RV
+		return
+	#
+# END FACTOR CLASS DEFINITION
 
-	def __imul__(self, other):
-		return self * other
 
-
+''' Compute Partition Function
+	@input 	factors		An array of Factor objects representing the graph
+	@return [float]		The partition function ( why is it called a function? )
+'''
+def computePartitionFunction( factors ):
+	# TODO: Implement a faster way to computer partition function by summing out variables
+	f = reduce( Factor.__mul__, factors )
+	z = sum( f.vals )
+	return z
 #
-# MAIN PROGRAM
-#
 
+''' Main '''
 def main():
-
-	A = 0
-	B = 1
-	C = 2
-	D = 3
-
+	# Cards {A:2, B:3, C:2}
 	global global_card
-	global_card = [ 2, 2, 2, 2 ]
+	global_card = [2, 3, 2]
 
-	f1 = Factor( [ A ], 	[ 1.5, 1.5 ] )
-	f2 = Factor( [ D, B ], 	[ 0.3, 0.7, 2.9, 0.1 ] )
+	# Computer partition function
+	z = computePartitionFunction( factors )
 
-	f3 = f1 * f2
-
-	print( f3 )
-
-
+	# Print results
+	print( "Z =", z )
 	return
 
-main()
+# Run main if this module is being run directly
+if __name__ == '__main__':
+	main()
